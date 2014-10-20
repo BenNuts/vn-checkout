8ae1aa87e977e2fad2f16686cd22762daaf8b81a
Tsanko Tsolov <tsanko_tsolov@volusion.com>
2014-10-20 14:22:39 -0700

    Move popover directive into toolbox-common.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -43,6 +43,26 @@ module.exports = function (config) {
 		// list of files / patterns to exclude
 		exclude   : [],
 
+		ngHtml2JsPreprocessor: {
+			// strip this from the file path
+			stripPrefix: 'app/scripts',
+			// prepend this to the
+			prependPrefix: '',
+
+			// or define a custom transform function
+			//cacheIdFromPath: function(filepath) {
+			//	return cacheId;
+			//},
+
+			// setting this option will create only a single module that contains templates
+			// from all the files, so you can load them all with module('foo')
+			moduleName: 'Volusion.Checkout.templates'
+		},
+
+		preprocessors: {
+			'app/scripts/**/*.html': ['ng-html2js']
+		},
+
 		// web server port
 		port      : 8080,
 
